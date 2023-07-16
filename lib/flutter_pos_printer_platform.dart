0005248e0cb6a6b6ed6857a063a848fec85f27fa
ramdasoutlogics <84704644+ramdasoutlogics@users.noreply.github.com>
2023-07-17 03:06:15 +1200

    * changes

--- lib/flutter_pos_printer_platform.dart
+++ lib/flutter_pos_printer_platform.dart
@@ -5,7 +5,6 @@ export './src/connectors/bluetooth.dart';
 export './src/utils.dart';
 export './src/connectors/tcp.dart';
 export './src/printers/escpos.dart';
-export './src/printers/star.dart';
 export './src/printers/tspl.dart';
 export './printer.dart';
 export './src/printer_manager.dart';
